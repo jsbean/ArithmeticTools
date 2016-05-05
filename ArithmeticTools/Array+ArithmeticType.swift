68e5283b24d47933c0c8e97911f295c2a6c2a6aa
jsbean <jbean46@gmail.com>
2016-05-05 12:18:54 -0400

    Remove combinations -> move to ArrayTools

--- ArithmeticTools/Array+ArithmeticType.swift
+++ ArithmeticTools/Array+ArithmeticType.swift
@@ -79,10 +79,3 @@ extension Array where Element: ArithmeticType {
         return cur
     }
 }
-
-/**
- - returns: All combinations of elements of two arrays.
- */
-public func combinations<T, U>(array1: [T], _ array2: [U]) -> [(T, U)] {
-    return array1.reduce([]) { accum, t in accum + array2.map { u in (t,u) } }
-}
--- ArithmeticToolsTests/SequenceTypeTests.swift
+++ ArithmeticToolsTests/SequenceTypeTests.swift
@@ -57,10 +57,4 @@ class SequenceTypeTests: XCTestCase {
         let closest = array.closest(to: 6)
         XCTAssertEqual(closest, 7)
     }
-    
-    func testCombinations() {
-        let array1 = [1,2,3]
-        let array2 = [4,5]
-        XCTAssertEqual(combinations(array1, array2).count, 6)
-    }
 }
