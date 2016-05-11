f74f0b84c4167d1f6825d37cd73f92dc0d89cb6d
jsbean <jbean46@gmail.com>
2016-05-11 14:18:39 -0400

    Create Comparison enum

---
+++ ArithmeticTools/Comparison.swift
@@ -0,0 +1,15 @@
+//
+//  Comparison.swift
+//  ArithmeticTools
+//
+//  Created by James Bean on 5/11/16.
+//  Copyright © 2016 James Bean. All rights reserved.
+//
+
+import Foundation
+
+public enum Comparison {
+    case equal
+    case lessThan
+    case greaterThan
+}
---
+++ ArithmeticToolsTests/ComparisonTests.swift
@@ -0,0 +1,15 @@
+//
+//  ComparisonTests.swift
+//  ArithmeticTools
+//
+//  Created by James Bean on 5/11/16.
+//  Copyright © 2016 James Bean. All rights reserved.
+//
+
+import XCTest
+@testable import ArithmeticTools
+
+class ComparisonTests: XCTestCase {
+    
+    //
+}
