94587e38089e6da75f92178f8f733b5679876d1c
Brian Heim <brian@lovelocaldesign.com>
2016-12-11 19:32:09 -0500

    Add tests for IsPowerOfTwo

---
+++ ArithmeticToolsTests/IsPowerOfTwoTests.swift
@@ -0,0 +1,39 @@
+//
+//  IsPowerOfTwoTests.swift
+//  ArithmeticTools
+//
+//  Created by Brian Heim on 12/11/16.
+//  Copyright © 2016 James Bean. All rights reserved.
+//
+
+import XCTest
+@testable import ArithmeticTools
+
+class PrimeTests: XCTestCase {
+    
+    func testIntIsPowerOfTwoTrue() {
+        XCTAssertTrue(1.isPowerOfTwo)
+        XCTAssertTrue(2.isPowerOfTwo)
+        XCTAssertTrue(4.isPowerOfTwo)
+        XCTAssertTrue(1024.isPowerOfTwo)
+        XCTAssertTrue(-1.isPowerOfTwo)
+        XCTAssertTrue(-2.isPowerOfTwo)
+        XCTAssertTrue(-4.isPowerOfTwo)
+        XCTAssertTrue(-1024.isPowerOfTwo)
+    }
+    
+    func testIntIsPowerOfTwoFalse() {
+        XCTAssertFalse(3.isPowerOfTwo)
+        XCTAssertFalse(17.isPowerOfTwo)
+        XCTAssertFalse(999.isPowerOfTwo)
+        XCTAssertFalse(1028.isPowerOfTwo)
+        XCTAssertFalse(-3.isPowerOfTwo)
+        XCTAssertFalse(-17.isPowerOfTwo)
+        XCTAssertFalse(-999.isPowerOfTwo)
+        XCTAssertFalse(-1028.isPowerOfTwo)
+    }
+    
+    func testIntIsPowerOfTwoZero() {
+        XCTAssertFalse(0.isPowerOfTwo)
+    }
+}
