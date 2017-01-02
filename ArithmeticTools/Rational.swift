cdd27c634b57b9147f5d71eded73707b4c429f5f
James <jbean46@gmail.com>
2017-01-02 22:53:24 +0100

    Create initial fields

--- ArithmeticTools/Rational.swift
+++ ArithmeticTools/Rational.swift
@@ -6,3 +6,27 @@
 //  Copyright © 2017 James Bean. All rights reserved.
 //
 
+public protocol Rational:
+    ExpressibleByIntegerLiteral,
+    ExpressibleByFloatLiteral,
+    Comparable,
+    Hashable,
+    CustomStringConvertible
+{
+    var numerator: Int { get }
+    var denominator: Int { get }
+    var floatValue: Float { get }
+    var inverse: Self { get }
+    var reduced: Self { get }
+    
+    init(_ numerator: Int, _ denominator: Int)
+}
+
+extension Rational {
+    
+    static func == (lhs: Self, rhs: Self) -> Bool {
+        fatalError("Not yet implemented")
+    }
+}
+
+// +(=) / -(=) / *(=) / \/(=)
