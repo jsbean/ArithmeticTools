12d989ab7192971bc0abcc9043e2a37c9b22bf4b
James <jbean46@gmail.com>
2017-01-02 22:45:27 +0100

    Create stubs for Rational protocol and tests

---
+++ ArithmeticToolsTests/RationalTests.swift
@@ -0,0 +1,15 @@
+//
+//  RationalTests.swift
+//  ArithmeticTools
+//
+//  Created by James Bean on 1/2/17.
+//  Copyright © 2017 James Bean. All rights reserved.
+//
+
+import XCTest
+import ArithmeticTools
+
+class RationalTests: XCTestCase {
+
+    
+}
