7cf1212eb508b128a431a9572a83d5c3eb00a63c
James <jbean46@gmail.com>
2017-02-09 13:45:35 -0500

    Add tests for odd count invertible sets

--- ArithmeticToolsTests/InvertibleOptionSetTests.swift
+++ ArithmeticToolsTests/InvertibleOptionSetTests.swift
@@ -11,12 +11,12 @@ import ArithmeticTools
 
 class InvertibleOptionSetTests: XCTestCase {
     
-    struct InvertibleSet: InvertibleOptionSet {
+    struct EvenCountInvertibleSet: InvertibleOptionSet {
         
-        static let a = InvertibleSet(rawValue: 1 << 0)
-        static let b = InvertibleSet(rawValue: 1 << 1)
-        static let c = InvertibleSet(rawValue: 1 << 2)
-        static let d = InvertibleSet(rawValue: 1 << 3)
+        static let a = EvenCountInvertibleSet(rawValue: 1 << 0)
+        static let b = EvenCountInvertibleSet(rawValue: 1 << 1)
+        static let c = EvenCountInvertibleSet(rawValue: 1 << 2)
+        static let d = EvenCountInvertibleSet(rawValue: 1 << 3)
         
         var optionsCount: Int {
             return 4
@@ -29,21 +29,63 @@ class InvertibleOptionSetTests: XCTestCase {
         }
     }
     
-    func testAInverseOfD() {
+    struct OddCountInvertibleSet: InvertibleOptionSet {
         
-        let a = InvertibleSet.a
-        let d = InvertibleSet.d
+        static let a = OddCountInvertibleSet(rawValue: 1 << 0)
+        static let b = OddCountInvertibleSet(rawValue: 1 << 1)
+        static let c = OddCountInvertibleSet(rawValue: 1 << 2)
+        static let d = OddCountInvertibleSet(rawValue: 1 << 3)
+        static let e = OddCountInvertibleSet(rawValue: 1 << 4)
+        
+        var optionsCount: Int {
+            return 5
+        }
+        
+        var rawValue: Int
+        
+        init(rawValue: Int) {
+            self.rawValue = rawValue
+        }
+    }
+    
+    func testEvenAInverseOfD() {
+        
+        let a = EvenCountInvertibleSet.a
+        let d = EvenCountInvertibleSet.d
         
         XCTAssertEqual(a.inverse, d)
         XCTAssertEqual(d.inverse, a)
     }
     
-    func testBInverseOfC() {
+    func testEvenBInverseOfC() {
         
-        let b = InvertibleSet.b
-        let c = InvertibleSet.c
+        let b = EvenCountInvertibleSet.b
+        let c = EvenCountInvertibleSet.c
         
         XCTAssertEqual(b.inverse, c)
         XCTAssertEqual(c.inverse, b)
+    }
+    
+    func testOddAInverseOfE() {
+        
+        let a = OddCountInvertibleSet.a
+        let e = OddCountInvertibleSet.e
+        
+        XCTAssertEqual(a.inverse, e)
+        XCTAssertEqual(e.inverse, a)
+    }
+    
+    func testOddBInverseOfD() {
+        
+        let b = OddCountInvertibleSet.b
+        let d = OddCountInvertibleSet.d
+        
+        XCTAssertEqual(b.inverse, d)
+        XCTAssertEqual(d.inverse, b)
+    }
+    
+    func testOddCInverseOfC() {
+        let c = OddCountInvertibleSet.c
+        XCTAssertEqual(c.inverse, c)
     }
 }
