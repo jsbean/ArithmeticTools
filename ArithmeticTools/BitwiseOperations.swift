040278e18d8ce2064f27b0652bd3f2b4716a602f
James <jbean46@gmail.com>
2017-02-09 13:59:11 -0500

    Remove old terrible invert(powerOfTwo:within)

--- ArithmeticTools/BitwiseOperations.swift
+++ ArithmeticTools/BitwiseOperations.swift
@@ -8,24 +8,6 @@
 
 import Foundation
 
-/// Log base 2 of an Integer.
-///
-/// - warning: This is only accurate if you know this a power-of-two!
-public func log2(powerOfTwo x: Int) -> Int {
-    return Int(log2(Float(x)))
-}
-
-/// Takes an integer that is a power-of-two (e.g., `1 << 3`).
-/// This retrieves the right-hand-side value, then inverts this value in relation to the given
-/// `max` value. The returned value can then be re-shifted as necessary.
-///
-///     invert(powerOfTwo: 1 << 3, within: 4) // 1
-///
-public func invert(powerOfTwo: Int, within max: Int) -> Int {
-    let unrolled = log2(powerOfTwo: powerOfTwo)
-    return max - unrolled
-}
-
 private let intBitCount = MemoryLayout<Int>.size * 8
 
 /// Count Trailing Zeros (ctz) counts the number of zero bits succeeding the least
