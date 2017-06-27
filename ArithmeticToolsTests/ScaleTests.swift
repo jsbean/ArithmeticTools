4f051af77263d7e6af847ad6f1b4f73b75eb8537
Brian Heim <brianlheim@gmail.com>
2017-06-26 21:44:05 -0400

    Add Double.scaleLinearToExponential w/ tests

--- ArithmeticToolsTests/ScaleTests.swift
+++ ArithmeticToolsTests/ScaleTests.swift
@@ -23,4 +23,17 @@ class ScaleTests: XCTestCase {
         let value: Float = 0.5
         XCTAssertEqual(value.scaled(from: sourceRange, to: destinationRange), 50)
     }
+
+    func testDoubleScaledLinearToExponential() {
+        let source = 0.0...1.0
+        let destination = 1.0...2.0
+        let values = [0.0, 0.5, 1.0]
+        let expecteds = [1.0, sqrt(2), 2]
+
+        for (value, expected) in zip(values, expecteds) {
+            let result = value.scaledLinearToExponential(from: source, to: destination)
+            XCTAssertEqual(result, expected)
+        }
+    }
+
 }
