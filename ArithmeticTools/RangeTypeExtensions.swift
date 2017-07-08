7ef641e58416858ec4d77d9139c4d16cbf0ac413
Brian Heim <brianlheim@gmail.com>
2017-07-08 16:59:25 -0400

    Add new RangeTypeExtensions file

---
+++ ArithmeticTools/RangeTypeExtensions.swift
@@ -0,0 +1,11 @@
+//
+//  RangeTypeExtensions.swift
+//  ArithmeticTools
+//
+//  Created by Brian Heim on 7/8/17.
+//  Copyright © 2017 Brian Heim. All rights reserved.
+//
+
+extension Range {
+    
+}
